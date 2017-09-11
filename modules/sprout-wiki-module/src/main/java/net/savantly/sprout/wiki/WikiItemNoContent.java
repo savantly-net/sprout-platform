e8422a505826259ca52b3cf094ec1e23b90c0e86
Jeremy <jdbranham@hotmail.com>
2017-09-11 12:42:06 -0500

    fixed projection, added FE maven plugin

--- modules/sprout-wiki-module/src/main/java/net/savantly/sprout/wiki/WikiItemNoContent.java
+++ modules/sprout-wiki-module/src/main/java/net/savantly/sprout/wiki/WikiItemNoContent.java
@@ -1,5 +1,8 @@
 package net.savantly.sprout.wiki;
 
+import org.springframework.data.rest.core.config.Projection;
+
+@Projection(types={WikiItem.class})
 public interface WikiItemNoContent {
 	
 	String getId();
--- modules/sprout-wiki-module/src/main/java/net/savantly/sprout/wiki/repository/WikiItemRepository.java
+++ modules/sprout-wiki-module/src/main/java/net/savantly/sprout/wiki/repository/WikiItemRepository.java
@@ -2,6 +2,7 @@ package net.savantly.sprout.wiki.repository;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
@@ -13,5 +14,6 @@ import net.savantly.sprout.wiki.WikiItemNoContent;
 @RepositoryRestResource(path="sprout-wiki")
 public interface WikiItemRepository extends PagingAndSortingRepository<WikiItem, String>{
 
-	Page<WikiItemNoContent> title(Pageable page);
+	@Query("SELECT w FROM WikiItem w")
+	public Page<WikiItemNoContent> allWithNoContent(Pageable page);
 }
