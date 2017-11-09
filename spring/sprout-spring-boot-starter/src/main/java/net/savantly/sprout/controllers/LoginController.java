354161094bf51da04eeb39f671fcf97b90461650
Jeremy <jdbranham@hotmail.com>
2017-11-09 14:50:18 -0600

    fixed freemarker configuration, where it was overwriting with defaults

--- spring/sprout-spring-boot-starter/src/main/java/net/savantly/sprout/controllers/LoginController.java
+++ spring/sprout-spring-boot-starter/src/main/java/net/savantly/sprout/controllers/LoginController.java
@@ -8,8 +8,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.HttpRequestResponseHolder;
 import org.springframework.security.web.context.SecurityContextRepository;
+import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
+import org.springframework.security.web.savedrequest.SavedRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -17,6 +21,7 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
+	HttpSessionRequestCache cache = new HttpSessionRequestCache();
 
 	@Autowired
 	AuthenticationManager authenticationManager;
@@ -27,17 +32,27 @@ public class LoginController {
 	public String login(HttpServletRequest request, HttpServletResponse response,
 			@RequestParam("username") String username, @RequestParam("password") String password)
 			throws ServletException {
+		
+		cache.saveRequest(request, response);
+		
 		Authentication result = this.authenticationManager
 				.authenticate(new UsernamePasswordAuthenticationToken(username, password));
 
-		SecurityContextHolder.getContext().setAuthentication(result);
+		HttpRequestResponseHolder holder = new  HttpRequestResponseHolder(request, response);
+		SecurityContext securityContext = securityContextRepository.loadContext(holder);
+		securityContext.setAuthentication(result);
 
-		this.securityContextRepository.saveContext(SecurityContextHolder.getContext(), request, response);
+		this.securityContextRepository.saveContext(securityContext, holder.getRequest(), holder.getResponse());
 
-		// TODO:
-		// SavedRequest savedRequest = securityContextRepository.loadContext(requestResponseHolder).getRequest(request, response);
-		return "redirect:/admin/";
-
+		
+		SavedRequest savedRequest = cache.getRequest(holder.getRequest(), holder.getResponse());
+		
+		if (savedRequest != null && savedRequest.getRedirectUrl() != null && !savedRequest.getRedirectUrl().endsWith("login")) {
+			String redirectUrl = savedRequest.getRedirectUrl();
+			return redirectUrl;
+		} else {
+			return "redirect:/admin/";
+		}
 	}
 
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
