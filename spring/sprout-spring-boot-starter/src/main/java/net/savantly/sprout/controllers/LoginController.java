3793c4dd696af516d8f95945df2c5e25548dcfb5
Jeremy Branham <jdbranham@hotmail.com>
2017-11-09 10:30:58 -0600

    login

--- spring/sprout-spring-boot-starter/src/main/java/net/savantly/sprout/controllers/LoginController.java
+++ spring/sprout-spring-boot-starter/src/main/java/net/savantly/sprout/controllers/LoginController.java
@@ -4,65 +4,50 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.springframework.security.core.AuthenticationException;
-import org.springframework.security.web.access.ExceptionTranslationFilter;
-import org.springframework.security.web.savedrequest.HttpSessionRequestCache;
-import org.springframework.security.web.savedrequest.RequestCache;
-import org.springframework.security.web.savedrequest.SavedRequest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.context.SecurityContextRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LoginController {
-	
-	private RequestCache requestCache = new HttpSessionRequestCache();
 
-    /**
-     * Authentication with Spring Security using {@link HttpServletRequest#login(String, String)}.
-     *
-     * <p>
-     * If we fail to authenticate, a {@link ServletException} is thrown that wraps the original
-     * {@link AuthenticationException} from Spring Security. This means we can catch the {@link ServletException} to
-     * display the error message. Alternatively, we could allow the {@link ServletException} to propagate and Spring
-     * Security's {@link ExceptionTranslationFilter} would catch it and process it appropriately.
-     * </p>
-     *
-     */
-    @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(HttpServletRequest request, HttpServletResponse response, 
-    		@RequestParam("username") String username,
-    		@RequestParam("password") String password,
-                        BindingResult result) throws ServletException {
-        try {
-            request.login(username, password);
-            SavedRequest savedRequest = requestCache.getRequest(request, response);
-            if (savedRequest != null) {
-                return "redirect:" + savedRequest.getRedirectUrl();
-            } else {
-                return "redirect:/";
-            }
+	@Autowired
+	AuthenticationManager authenticationManager;
+	@Autowired
+	SecurityContextRepository securityContextRepository;
 
-        } catch (ServletException authenticationFailed) {
-            result.rejectValue(null, "authentication.failed");
-            return "login";
-        }
-    }
+	@RequestMapping(value = "/login", method = RequestMethod.POST)
+	public String login(HttpServletRequest request, HttpServletResponse response,
+			@RequestParam("username") String username, @RequestParam("password") String password)
+			throws ServletException {
+		Authentication result = this.authenticationManager
+				.authenticate(new UsernamePasswordAuthenticationToken(username, password));
 
-    @RequestMapping(value = "/login", method = RequestMethod.GET)
-    public String login() {
-        return "login";
-    }
+		SecurityContextHolder.getContext().setAuthentication(result);
 
-    /**
-     * Demonstrates that invoking {@link HttpServletRequest#logout()} will log the user out.
-     * We then redirect the user to the home page.
-     */
-    @RequestMapping("/logout")
-    public String logout(HttpServletRequest request) throws ServletException {
-    	request.logout();
-        return "redirect:/";
-    }
+		this.securityContextRepository.saveContext(SecurityContextHolder.getContext(), request, response);
+
+		// TODO:
+		// SavedRequest savedRequest = securityContextRepository.loadContext(requestResponseHolder).getRequest(request, response);
+		return "redirect:/admin/";
+
+	}
+
+	@RequestMapping(value = "/login", method = RequestMethod.GET)
+	public String login() {
+		return "login";
+	}
+
+	@RequestMapping("/logout")
+	public String logout(HttpServletRequest request) throws ServletException {
+		request.logout();
+		return "redirect:/";
+	}
 }
