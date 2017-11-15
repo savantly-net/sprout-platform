8604f035f7366ea81af7b9bfc6a20f83c4d78b0a
Jeremy Branham <jdbranham@hotmail.com>
2017-11-15 08:25:40 -0600

    added role management

--- web/sprout-admin/src/app/user/role.service.ts
+++ web/sprout-admin/src/app/user/role.service.ts
@@ -1,16 +1,39 @@
 import { HalResponse, RestRepositoryService } from '../spring-data/rest-repository.service';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs/Observable';
 
-export class Role extends HalResponse {
+export class Role {
   authority: string;
+  _links: any;
+  _embedded: any;
 }
 
 @Injectable()
-export class RoleService extends RestRepositoryService<Role> {
+export class RoleService {
+  baseRepositoryPath = '/api/roles';
 
-  constructor(http: HttpClient) {
-    super(http, '/api/roles');
+  findAll(options?: any): Observable<any> {
+    return this.http.get(this.baseRepositoryPath, options);
   }
+
+  findOne(id: string, options?: any): Observable<any> {
+    return this.http.get(this.baseRepositoryPath + '/' + id, options);
+  }
+
+  saveItem(item: Role): Observable<any> {
+    console.info('attempting to save object: {}', item);
+    if (item['new'] === false) {
+      return this.http.put(this.baseRepositoryPath + '/' + item.authority, item);
+    } else {
+      return this.http.post(this.baseRepositoryPath, item);
+    }
+  }
+
+  deleteItem(item: Role): Observable<any> {
+    return this.http.delete(this.baseRepositoryPath + '/' + item.authority);
+  }
+
+  constructor(protected http: HttpClient) { }
 
 }
