55ef0b4e36efbf7805ab5bfba57a54424dc934ff
Jeremy <jdbranham@hotmail.com>
2017-12-04 16:52:55 -0600

    updtes

--- web/sprout-web-ui/src/app/settings/settings.service.ts
+++ web/sprout-web-ui/src/app/settings/settings.service.ts
@@ -1,26 +1,40 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { BehaviorSubject } from 'rxjs';
+
+export class Setting {
+  id: string;
+  value: string;
+}
+export class Settings {
+  siteTitle: Setting;
+  siteDescription: Setting;
+  siteUrl: Setting;
+  keywords: Setting;
+  siteName: Setting;
+  siteImage: Setting;
+}
 
 @Injectable()
 export class SettingsService {
 
-  value: BehaviorSubject<any> = new BehaviorSubject<any>({});
+  value: Promise<Settings>;
 
   constructor(http: HttpClient) {
-    http.get('/rest/client/config').subscribe((response) => {
-      this.value.next(response);
-    }, (err) => {
-      console.warn('Ignore if running outside the Sprout platform: failed to retrieve config from server.', err);
-      console.log('Using client configuration defaults');
-      this.value.next({
-        siteTitle: 'Sprout App',
-        siteDescription: 'My Awesome Sprout Application',
-        siteUrl: window.location.href,
-        keywords: 'sprout, spring, cms',
-        siteName: 'Sprout site',
-        siteImage: './img/sprout.png'
-      });
+    this.value = new Promise((resolve) => {
+        http.get('/rest/client/config').subscribe((response: Settings) => {
+          resolve(response);
+        }, (err) => {
+          console.warn('Ignore if running outside the Sprout platform: failed to retrieve config from server.', err);
+          console.log('Using client configuration defaults');
+          resolve({
+            siteTitle: 'Sprout App',
+            siteDescription: 'My Awesome Sprout Application',
+            siteUrl: window.location.href,
+            keywords: 'sprout, spring, cms',
+            siteName: 'Sprout site',
+            siteImage: './img/sprout.png'
+          });
+        });
     });
   }
 
