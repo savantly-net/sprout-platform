78a53913e069f9aca8dbebe400d735b905a21dec
Jeremy <jdbranham@hotmail.com>
2017-12-08 18:09:14 -0600

    added initial file browser client side

--- web/sprout-admin/src/app/app.component.ts
+++ web/sprout-admin/src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { AppService } from './app.service';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,7 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Sprout Admin';
+  hideToolbar = this.appService.getHideToolbar();
+
+  constructor (private appService: AppService) { }
 }
