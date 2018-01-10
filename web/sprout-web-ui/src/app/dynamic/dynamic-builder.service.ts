43afed21cb81f275a6914b3e737f4b2072e2a214
Jeremy <jdbranham@hotmail.com>
2018-01-10 14:53:41 -0600

    disable plugin html sanitizing

--- web/sprout-web-ui/src/app/dynamic/dynamic-builder.service.ts
+++ web/sprout-web-ui/src/app/dynamic/dynamic-builder.service.ts
@@ -1,15 +1,17 @@
 import { DynamicComponent } from './dynamic.component';
 import { Injectable, ViewContainerRef, ComponentFactoryResolver, ComponentRef } from '@angular/core';
+import { DomSanitizer } from '@angular/platform-browser';
 
 @Injectable()
 export class DynamicBuilderService {
 
-  constructor(private factoryResolver: ComponentFactoryResolver) { }
+  constructor(private factoryResolver: ComponentFactoryResolver, private _sanitizer: DomSanitizer) { }
 
   createComponent(template: string, viewContainerRef: ViewContainerRef): ComponentRef<DynamicComponent> {
     const factory = this.factoryResolver.resolveComponentFactory(DynamicComponent);
     const componentRef = factory.create(viewContainerRef.parentInjector);
-    componentRef.instance.body = template;
+    // Trust the html so tags aren't stripped. Risky but adds flexibility for plugins
+    componentRef.instance.body = this._sanitizer.bypassSecurityTrustHtml(template);
     viewContainerRef.insert(componentRef.hostView);
     return componentRef;
   }
