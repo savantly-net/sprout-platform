0ecc2d40a320237fbe13ae9f65aa89a6fd5d10f9
Jeremy <jdbranham@hotmail.com>
2018-02-10 19:18:25 -0600

    serach by contentitem

--- sprout-core/src/main/java/net/savantly/sprout/core/content/contentItem/ContentItemRepository.java
+++ sprout-core/src/main/java/net/savantly/sprout/core/content/contentItem/ContentItemRepository.java
@@ -2,9 +2,11 @@ package net.savantly.sprout.core.content.contentItem;
 
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+import java.util.Set;
 import net.savantly.sprout.core.domain.PersistedDomainObjectRepository;
 
 @RepositoryRestResource
 public interface ContentItemRepository extends PersistedDomainObjectRepository<ContentItem> {
 
+	public Set<ContentItem> findAllByContentType_id(String contentTypeId);
 }
