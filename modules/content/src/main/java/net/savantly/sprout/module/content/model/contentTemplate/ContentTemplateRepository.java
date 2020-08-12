980bd160aa7e550018c65679864cfdde4b8bd97c
Jeremy Branham <Jeremy@savantly.net>
2020-08-12 15:38:15 -0500

    include id serialization for entities

--- modules/content/src/main/java/net/savantly/sprout/module/content/model/contentTemplate/ContentTemplateRepository.java
+++ modules/content/src/main/java/net/savantly/sprout/module/content/model/contentTemplate/ContentTemplateRepository.java
@@ -1,5 +1,6 @@
 package net.savantly.sprout.module.content.model.contentTemplate;
 
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import net.savantly.sprout.core.domain.PersistedDomainObjectRepository;
@@ -7,5 +8,5 @@ import net.savantly.sprout.core.domain.PersistedDomainObjectRepository;
 @RepositoryRestResource(path="contentTemplates")
 public interface ContentTemplateRepository extends PersistedDomainObjectRepository<ContentTemplate>{
 
-	ContentTemplate findByName(String name);
+	ContentTemplate findByName(@Param("name") String name);
 }
--- modules/content/src/main/java/net/savantly/sprout/module/content/model/contentType/ContentTypeRepository.java
+++ modules/content/src/main/java/net/savantly/sprout/module/content/model/contentType/ContentTypeRepository.java
@@ -1,5 +1,6 @@
 package net.savantly.sprout.module.content.model.contentType;
 
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import net.savantly.sprout.core.domain.PersistedDomainObjectRepository;
@@ -7,5 +8,5 @@ import net.savantly.sprout.core.domain.PersistedDomainObjectRepository;
 @RepositoryRestResource(path="contentTypes")
 public interface ContentTypeRepository extends PersistedDomainObjectRepository<ContentType> {
 
-	ContentType findByName(String name);
+	ContentType findByName(@Param("name") String name);
 }
