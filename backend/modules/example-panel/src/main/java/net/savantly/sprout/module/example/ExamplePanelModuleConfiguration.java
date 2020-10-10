afc2a303d8bea7d64c3d3284381554d57be5127d
Jeremy Branham <Jeremy@savantly.net>
2020-10-10 18:01:01 -0500

    restrcture into BE/FE

---
+++ backend/modules/example-panel/src/main/java/net/savantly/sprout/module/example/ExamplePanelModuleConfiguration.java
@@ -0,0 +1,27 @@
+package net.savantly.sprout.module.example;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import lombok.Data;
+import net.savantly.sprout.core.module.SproutModuleConfiguration;
+import net.savantly.sprout.module.example.api.ExamplePanelApi;
+
+/**
+ * 
+ * An example of how the 'application.properties' values can be injected at runtime into other beans
+ *
+ */
+@Data
+@SproutModuleConfiguration
+@ConfigurationProperties("example")
+public class ExamplePanelModuleConfiguration {
+	
+	private String message = "";
+
+	@Bean
+	public ExamplePanelApi examplePanelApi() {
+		return new ExamplePanelApi(message);
+	}
+
+}
