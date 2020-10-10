785f8aed01ca2c080089128838a331585de0dddc
Jeremy Branham <Jeremy@savantly.net>
2020-10-10 07:54:46 -0500

    adds example app from grafana

--- modules/example-app/src/module.ts
+++ modules/example-app/src/module.ts
@@ -1,8 +1,28 @@
-import { PanelPlugin } from '@savantly/sprout-api';
-import { SimpleOptions } from './plugin/types';
+import { AppPlugin, AppRootProps, PanelPlugin } from '@savantly/sprout-api';
+import { ExampleRootPage } from 'plugin/ExampleRootPage';
+import { ComponentClass } from 'react';
 import { SimplePanel } from './plugin/SimplePanel';
+import { ExampleAppSettings, SimpleOptions } from './plugin/types';
+import { ExamplePage1 } from "./plugin/config/ExamplePage1";
+import { ExamplePage2 } from 'plugin/config/ExamplePage2';
 
-export const plugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOptions(builder => {
+
+  export const plugin = new AppPlugin<ExampleAppSettings>()
+  .setRootPage((ExampleRootPage as unknown) as ComponentClass<AppRootProps>)
+  .addConfigPage({
+    title: 'Page 1',
+    icon: 'fa fa-info',
+    body: ExamplePage1,
+    id: 'page1',
+  })
+  .addConfigPage({
+    title: 'Page 2',
+    icon: 'fa fa-user',
+    body: ExamplePage2,
+    id: 'page2',
+  });
+
+export const panelPlugin = new PanelPlugin<SimpleOptions>(SimplePanel).setPanelOptions(builder => {
   return builder
     .addTextInput({
       path: 'text',
--- modules/example-app/src/plugin/types.ts
+++ modules/example-app/src/plugin/types.ts
@@ -1,7 +1,14 @@
+import { PluginMeta } from '@savantly/sprout-api';
+
 type SeriesSize = 'sm' | 'md' | 'lg';
 
 export interface SimpleOptions {
   text: string;
   showMessage: boolean;
   messageSize: SeriesSize;
+}
+
+export interface ExampleAppSettings {
+  customText?: string;
+  customCheckbox?: boolean;
 }
