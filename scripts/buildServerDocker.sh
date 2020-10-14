1b5f923d435e53e4e569b2095710f25f1b9dd3f3
Jeremy Branham <Jeremy@savantly.net>
2020-10-14 14:26:13 -0500

    update docker for importing modules at runtime

--- scripts/buildServerDocker.sh
+++ scripts/buildServerDocker.sh
@@ -5,5 +5,8 @@ REPO_URL=savantly/sprout-server
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR/..
+./gradlew :sprout-server:build
 
-./gradlew :sprout-server:bootBuildImage --imageName=$REPO_URL
+cd ./backend/server
+
+docker build -t $REPO_URL .
