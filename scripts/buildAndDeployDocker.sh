83ede5c4be3dcb606f87567ca4b135c215a648e0
Jeremy Branham <Jeremy.Branham@gigsternetwork.com>
2021-04-15 08:10:50 -0500

    fix build

--- scripts/buildAndDeployDocker.sh
+++ scripts/buildAndDeployDocker.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-set -e
-
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 cd $DIR
 
 ./buildClientProjects.sh
+
+set -e
 ./buildClientDocker.sh
 ./buildServerDocker.sh
 ./deployDocker.sh
