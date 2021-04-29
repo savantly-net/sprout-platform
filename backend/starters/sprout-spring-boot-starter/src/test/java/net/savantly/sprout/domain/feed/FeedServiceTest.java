4f7832a91eaf3175d0e97e0d5275f5fc5af7cef4
Jeremy Branham <Jeremy.Branham@gigsternetwork.com>
2021-04-29 10:51:00 -0500

    reduce setup

--- backend/starters/sprout-spring-boot-starter/src/test/java/net/savantly/sprout/domain/feed/FeedServiceTest.java
+++ backend/starters/sprout-spring-boot-starter/src/test/java/net/savantly/sprout/domain/feed/FeedServiceTest.java
@@ -4,18 +4,25 @@ import java.time.ZonedDateTime;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import net.savantly.sprout.core.domain.feed.item.FeedItem;
 import net.savantly.sprout.domain.feed.post.FeedPostDto;
 import net.savantly.sprout.domain.feed.post.FeedPostService;
+import net.savantly.sprout.starter.security.context.SproutSecurityContext;
+import net.savantly.sprout.starter.security.context.SproutSecurityContextImpl;
 import net.savantly.sprout.test.AbstractContainerBaseTest;
-import net.savantly.sprout.test.IntegrationTest;
 
-@SpringBootTest
-@IntegrationTest
+@SpringJUnitConfig(classes = FeedServiceTest.TestContext.class)
+@AutoConfigureDataJpa
+@AutoConfigureTestEntityManager
 public class FeedServiceTest extends AbstractContainerBaseTest {
 
 	@Autowired
@@ -34,9 +41,16 @@ public class FeedServiceTest extends AbstractContainerBaseTest {
 		posts.deletePost(result.getId());
 	}
 
-	@Configuration
-	@EnableAutoConfiguration
+	@TestConfiguration
+	@Import(FeedConfiguration.class)
+	@EnableJpaRepositories(basePackages = "net.savantly.sprout.core.domain.feed")
+	@EntityScan(basePackages = {"net.savantly.sprout.core.domain.feed"})
 	static class TestContext {
+		
+		@Bean
+		public SproutSecurityContext sproutSecurityContext() {
+			return new SproutSecurityContextImpl();
+		}
 
 	}
 }
