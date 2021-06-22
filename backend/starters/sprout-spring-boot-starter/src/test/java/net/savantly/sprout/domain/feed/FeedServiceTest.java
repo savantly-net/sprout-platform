1311a4257b75c28f9bab7cd9afb6a5b678269b6e
Jeremy Branham <Jeremy@Savantly.net>
2021-06-22 15:44:12 -0500

    use h2 for test

--- backend/starters/sprout-spring-boot-starter/src/test/java/net/savantly/sprout/domain/feed/FeedServiceTest.java
+++ backend/starters/sprout-spring-boot-starter/src/test/java/net/savantly/sprout/domain/feed/FeedServiceTest.java
@@ -4,27 +4,27 @@ import java.time.ZonedDateTime;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
-import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureTestEntityManager;
-import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 import net.savantly.sprout.core.domain.feed.item.FeedItem;
 import net.savantly.sprout.domain.feed.post.FeedPostDto;
 import net.savantly.sprout.domain.feed.post.FeedPostService;
-import net.savantly.sprout.starter.migration.CoreDBMigrationConfiguration;
 import net.savantly.sprout.starter.security.context.SproutSecurityContext;
 import net.savantly.sprout.starter.security.context.SproutSecurityContextImpl;
-import net.savantly.sprout.test.AbstractContainerBaseTest;
 
-@SpringJUnitConfig(classes = FeedServiceTest.TestContext.class)
-@AutoConfigureDataJpa
-@AutoConfigureTestEntityManager
-public class FeedServiceTest extends AbstractContainerBaseTest {
+@SpringJUnitConfig
+@DataJpaTest
+@AutoConfigureTestDatabase
+@ActiveProfiles("h2")
+public class FeedServiceTest {
 
 	@Autowired
 	FeedService service;
@@ -42,8 +42,8 @@ public class FeedServiceTest extends AbstractContainerBaseTest {
 		posts.deletePost(result.getId());
 	}
 
-	@TestConfiguration
-	@Import({CoreDBMigrationConfiguration.class, FeedConfiguration.class})
+	@SpringBootConfiguration
+	@Import({FeedConfiguration.class})
 	@EnableJpaRepositories(basePackages = "net.savantly.sprout.core.domain.feed")
 	@EntityScan(basePackages = {"net.savantly.sprout.core.domain.feed"})
 	static class TestContext {
