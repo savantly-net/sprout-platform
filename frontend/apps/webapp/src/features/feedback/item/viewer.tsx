d5d9854e8acc7a688e20f4485c6902219b60e68d
Manoj <p.manoj.vivek+savantly@gmail.com>
2021-08-24 13:11:53 +0530

    Issue Entity Viewer Upgraded

--- frontend/apps/webapp/src/features/feedback/item/viewer.tsx
+++ frontend/apps/webapp/src/features/feedback/item/viewer.tsx
@@ -1,34 +1,58 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Card, CardBody } from 'reactstrap';
+import { Text, Heading, Divider, Stack, FormLabel, Table, Thead, Tr, Tbody, Td, Th, Tag } from '@chakra-ui/react';
 import { IssueEntity as EntityClass } from '../entity';
 
 export const IssueEntityViewer = ({ item }: { item: EntityClass }) => {
   return (
-    <Fragment>
-      <h2>{item.title}</h2>
-      <hr />
-      <div className="mb-2">
-        <h5>Details</h5>
-        <div>
-          <label className="mr-2">Title: </label>
-          <span>{item.description}</span>
-        </div>
-        <div>
-          <label className="mr-2">Description: </label>
-          <span>{item.description}</span>
-        </div>
-        <div>
-          <label className="mr-2">Comments: </label>
-          {item.comments && (item.comments.map(c => (
-            <Card>
-              <CardBody>
-                {c.text}
-              </CardBody>
-            </Card>
-          )))}
-
-        </div>
-      </div>
-    </Fragment>
+    <Stack spacing={6}>
+      <Heading as="h2" size="md">
+        {item.title}
+      </Heading>
+      <Divider />
+      <Stack spacing={3}>
+        <Table variant="simple">
+          <Thead>
+            <Tr>
+              <Th>Property</Th>
+              <Th>Value</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            <Tr>
+              <Td>Title</Td>
+              <Td>{item.title}</Td>
+            </Tr>
+            <Tr>
+              <Td>Description</Td>
+              <Td>{item.description}</Td>
+            </Tr>
+            <Tr>
+              <Td>Status</Td>
+              <Td>{item.status}</Td>
+            </Tr>
+            <Tr>
+              <Td>Tags</Td>
+              <Td>
+                {item.tags?.map((tag) => (
+                  <Tag key={tag}>{tag}</Tag>
+                ))}
+              </Td>
+            </Tr>
+            <Tr>
+              <Td>Comments</Td>
+              <Td>
+                {item.comments &&
+                  item.comments.map((c) => (
+                    <Card>
+                      <CardBody>{c.text}</CardBody>
+                    </Card>
+                  ))}
+              </Td>
+            </Tr>
+          </Tbody>
+        </Table>
+      </Stack>
+    </Stack>
   );
 };
