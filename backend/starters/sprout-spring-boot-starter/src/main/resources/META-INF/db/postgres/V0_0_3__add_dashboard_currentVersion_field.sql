6d294060a8cf6a8e8156ae9e2b9a1918120f44e7
Jeremy Branham <Jeremy@Savantly.net>
2021-09-23 23:27:50 -0500

    use correct exception code

--- backend/starters/sprout-spring-boot-starter/src/main/resources/META-INF/db/postgres/V0_0_3__add_dashboard_currentVersion_field.sql
+++ backend/starters/sprout-spring-boot-starter/src/main/resources/META-INF/db/postgres/V0_0_3__add_dashboard_currentVersion_field.sql
@@ -4,13 +4,13 @@ BEGIN
   BEGIN
     ALTER TABLE DASHBOARDS ADD COLUMN currentVersion boolean not null DEFAULT false;
   EXCEPTION
-    WHEN duplicate_object THEN RAISE NOTICE 'currentVersion column already exists on dashboards';
+    WHEN duplicate_column THEN RAISE NOTICE 'currentVersion column already exists on dashboards';
   END;
   BEGIN
     -- message existed in earlier version but was missed when switching to migrations
     ALTER TABLE DASHBOARDS ADD COLUMN message varchar(256);
   EXCEPTION
-    WHEN duplicate_object THEN RAISE NOTICE 'message column already exists on dashboards';
+    WHEN duplicate_column THEN RAISE NOTICE 'message column already exists on dashboards';
   END;
 
 END $$;
