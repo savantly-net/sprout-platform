b55d0e495154e68390f93ff3c4ce6903bc15517f
Jeremy Branham <jeremy@savantly.net>
2021-11-16 13:43:44 -0600

    remove unused code

--- frontend/apps/webapp/src/features/menu/DragAndDropMenuBuilder/AddMenuItem/index.tsx
+++ frontend/apps/webapp/src/features/menu/DragAndDropMenuBuilder/AddMenuItem/index.tsx
@@ -3,52 +3,15 @@ import {
   Popover,
   PopoverTrigger,
   PopoverContent,
-  PopoverHeader,
-  PopoverBody,
-  PopoverFooter,
   PopoverArrow,
   PopoverCloseButton,
-  FormControl,
-  FormLabel,
-  Input,
-  Stack,
-  ButtonGroup,
   Button,
-  Box,
-  IconButton,
   useDisclosure,
   useToast
 } from '@chakra-ui/react';
 import FocusLock from 'react-focus-lock';
 import MenuItem from '../DraggableMenuItem/MenuItem';
 import { MenuDto } from '../../menuAdminService';
-
-const TextInput = React.forwardRef((props, ref) => {
-  return (
-    <FormControl>
-      <FormLabel htmlFor={props.id}>{props.label}</FormLabel>
-      <Input ref={ref} id={props.id} {...props} />
-    </FormControl>
-  );
-});
-
-// 2. Create the form
-const Form = ({ firstFieldRef, onCancel }) => {
-  return (
-    <Stack spacing={4}>
-      <TextInput label="First name" id="first-name" ref={firstFieldRef} defaultValue="John" />
-      <TextInput label="Last name" id="last-name" defaultValue="Smith" />
-      <ButtonGroup d="flex" justifyContent="flex-end">
-        <Button variant="outline" onClick={onCancel}>
-          Cancel
-        </Button>
-        <Button isDisabled colorScheme="teal">
-          Save
-        </Button>
-      </ButtonGroup>
-    </Stack>
-  );
-};
 
 interface Props {
   onSave: (menu: MenuDto) => void;
