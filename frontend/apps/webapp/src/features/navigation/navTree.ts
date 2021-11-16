97127d00a6ed7df293ba51de7c7c8023b7575b4e
Jeremy Branham <jeremy@savantly.net>
2021-11-16 13:44:10 -0600

    fix type constraints

--- frontend/apps/webapp/src/features/navigation/navTree.ts
+++ frontend/apps/webapp/src/features/navigation/navTree.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk, createSlice, PayloadAction } from '@reduxjs/toolkit';
 import { NavModelItem, NavModelItemRenderMode } from '@savantly/sprout-api';
+import { WritableDraft } from '../../../../../../common/temp/node_modules/.pnpm/registry.npmjs.org/immer/7.0.14/node_modules/immer/dist/internal';
 import { SERVER_API_URL } from '../../config/constants';
 import { sproutApiSvc } from '../../core/services/sproutApiSvc';
 import { defaultNavTree } from './defaultNavTree';
@@ -28,6 +29,28 @@ function toNavModel(menuItem: ServerMenuItem): NavModelItem {
   };
 }
 
+function navModelItemFromWritableDraft(draft: WritableDraft<NavModelItem>): NavModelItem {
+  return {
+    text: draft.text,
+    active: draft.active,
+    authority: draft.authority,
+    breadcrumbs: draft.breadcrumbs,
+    children: draft.children?.map(dc => navModelItemFromWritableDraft(dc)),
+    divider: draft.divider,
+    hideFromMenu: draft.hideFromMenu,
+    hideFromTabs: draft.hideFromTabs,
+    icon: draft.icon as any,
+    id: draft.id,
+    img: draft.img,
+    onClick: draft.onClick,
+    position: draft.position,
+    renderMode: draft.renderMode,
+    subTitle: draft.subTitle,
+    target: draft.target,
+    url: draft.url
+  }
+}
+
 export type NavTreeState = {
   items: NavModelItem[];
   fetching: boolean;
@@ -50,13 +73,13 @@ const navTreeSlice = createSlice({
   name: 'navTree',
   initialState,
   reducers: {
-    addRootNav: (state, action: PayloadAction<NavModelItem>): NavTreeState => ({
+    addRootNav: (state: WritableDraft<NavTreeState>, action: PayloadAction<NavModelItem>): NavTreeState => ({
       ...state,
-      items: state.items.concat([action.payload])
+      items: state.items.map(d => navModelItemFromWritableDraft(d))
     }),
     addRootNavs: (state, action: PayloadAction<NavModelItem[]>): NavTreeState => ({
       ...state,
-      items: state.items.concat(action.payload)
+      items: state.items.map(d => navModelItemFromWritableDraft(d))
     }),
     updateNavTree: (state, action: PayloadAction<NavModelItem[]>): NavTreeState => ({
       ...state,
@@ -64,25 +87,25 @@ const navTreeSlice = createSlice({
     })
   },
   extraReducers: (builder) => {
-    builder.addCase(loadNavTreeState.pending, (state, action) => {
+    builder.addCase(loadNavTreeState.pending, (state, action): NavTreeState => {
       return {
-        ...state,
+        items: state.items.map(d => navModelItemFromWritableDraft(d)),
         fetched: false,
         fetching: true,
         error: ''
       };
     });
-    builder.addCase(loadNavTreeState.fulfilled, (state, action) => {
+    builder.addCase(loadNavTreeState.fulfilled, (state, action): NavTreeState => {
       return {
-        items: state.items.concat(action.payload.data.map((n) => toNavModel(n))),
+        items: state.items.map(d => navModelItemFromWritableDraft(d)),
         fetched: true,
         fetching: false,
         error: ''
       };
     });
-    builder.addCase(loadNavTreeState.rejected, (state, action) => {
+    builder.addCase(loadNavTreeState.rejected, (state, action): NavTreeState => {
       return {
-        ...state,
+        items: state.items.map(d => navModelItemFromWritableDraft(d)),
         fetched: false,
         fetching: false,
         error: action.error.message || 'failed for unknown reason'
