689e02c244563d28a70a0bc4f52f6d7e21baaf86
Jeremy <jeremy@savantly.net>
2022-05-10 21:56:15 -0500

    update FE deps

--- backend/modules/newsfeed/src/plugin/config/ExamplePage1.tsx
+++ backend/modules/newsfeed/src/plugin/config/ExamplePage1.tsx
@@ -1,5 +1,5 @@
 import { AppPluginMeta, PluginConfigPageProps } from '@savantly/sprout-api';
-import { getBackendSrv, BackendSrv } from '@savantly/sprout-runtime';
+import { getApiService, ApiService } from '@savantly/sprout-runtime';
 import React, { PureComponent } from 'react';
 import { ExampleAppSettings } from '../types';
 
@@ -9,10 +9,10 @@ interface State {
 }
 
 export class ExamplePage1 extends PureComponent<Props, State> {
-  backendSvc: BackendSrv;
+  backendSvc: ApiService;
   constructor(props: Props) {
     super(props);
-    this.backendSvc = getBackendSrv();
+    this.backendSvc = getApiService();
     this.state = {
       message: 'Waiting for server...',
     };
@@ -25,7 +25,7 @@ export class ExamplePage1 extends PureComponent<Props, State> {
   getMessageFromExampleApi = () => {
     this.backendSvc.get('/api/example-app').then(value => {
       this.setState({
-        message: value,
+        message: value.data,
       });
     });
   };
